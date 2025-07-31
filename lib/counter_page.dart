ff74caaa2c1a758b88962c90540da3f2fb176372
Chhyoti <122035981+Chhyoti@users.noreply.github.com>
2025-07-31 23:04:02 +0545

    UI changed

--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -10,13 +10,36 @@ class CounterPage extends StatelessWidget {
     final counter = Provider.of<CounterProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Provider Counter')),
+      backgroundColor: const Color(0xFFF5F5F5),
+      appBar: AppBar(
+        title: const Text(' Provider Counter'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
       body: Center(
-        child: Text('Count: ${counter.count}', style: TextStyle(fontSize: 30)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Current Count:',
+              style: TextStyle(fontSize: 24, color: Colors.grey),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${counter.count}',
+              style: const TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: counter.increment,
-        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
